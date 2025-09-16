1ea3f596c6b63c067679b98fbc22175e0ccc636b
I <2430832@cognizant.com>
2025-09-16 15:45:17 +0530

    Todo Angular Quater Done

---
+++ src/app/Todos/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'app-todo-list-item',
+  templateUrl: './todo-list-item.component.html',
+  styleUrls: ['./todo-list-item.component.css']
+})
+export class TodoListItemComponent {
+@Input() title:string="";
+
+//Declaration of isComplete as a signal
+isComplete = signal<boolean>(false);
+isNotComplete = signal<boolean>(true);
+
+//Disable button when the task is completed
+buttonDisable(){
+  return this.isComplete();
+}
+
+//Status of the task
+staus: Status = Status.ToDo;
+
+toggleCompleted(){
+  this.isComplete.update((currentValue)=>!currentValue);
+  this.staus = this.isComplete() ? Status.Completed : Status.ToDo;
+}
+
+toggleIsNotCompleted(){
+  this.isNotComplete.set(false);  
+  this.staus = Status.Completed;
+}
+
+toggleCancel(){
+  this.isComplete.set(false);
+  this.staus = Status.NeedToStart;
+}
+ 
+
+
+inputValue: string = '';
+displayValue: string = '';
+
+  updateDisplay() {
+    this.displayValue = this.inputValue;
+  }
+}
+
+enum Status {
+  ToDo = "ToDo",
+  NeedToStart = "Need To Start",
+  InProgress = "In Progress",
+  Completed = "Completed"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './Todos/todo-list/todo-list.component';
+import { TodoListItemComponent } from './Todos/todo-list-item/todo-list-item.component';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
